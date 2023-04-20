e8f835270c891cba62cf0f1c961d70cd6ba4272d
Alina Iusupova <alina.usupova@gmail.com>
2023-04-20 23:38:48 +0300

    task 2/1

--- task_2/solution.js
+++ task_2/solution.js
@@ -15,6 +15,16 @@ function calcShipping(sum, min, shipping) {
 
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
+    let shippingSum;
+    if (productsSum = 0){
+        shippingSum = 0
+    }
+    if (productsSum >= freeShippingMinSum){
+        shippingSum = 0
+    }
+    if (productsSum == 0 || productsSum >= freeShippingMinSum){
+        shippingSum = shippingPrice
+    }    
 
     // Конец решения задания №2.1.
 
