b9df40aa3800e91cdff28a2d9f03d8887caf352c
Alina Iusupova <alina.usupova@gmail.com>
2023-04-21 01:10:07 +0300

    task 2/2

--- task_2/solution.js
+++ task_2/solution.js
@@ -40,6 +40,12 @@ function calcDiscount(sum, min, discount) {
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
+    let discountSum;
+    if (productsSum >= discountMinSum){
+        discountSum = productsSum / 100 * discountPart;
+    }else{
+        discountPart = 0;
+    }
     
     // Конец решения задания №2.2.
 
